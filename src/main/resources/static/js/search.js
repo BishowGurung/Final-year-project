669194e973237b506f71e0f94b97ff491abaf6d5
21441393 <Rfn_Ghonde@@5>
2023-11-05 19:29:12 +0000

    js added

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,7 +1,19 @@
 function getValue(){
     var nameValue = document.getElementById("querySearch").value;
+    document.getElementById("querySearch").value = "";
     $(".aside-search-result-list").empty();
     $(".aside-search-result-list").append(nameValue);
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "http://localhost:8080/api/?query="+nameValue);
+
+    xhr.send();
 }
 
